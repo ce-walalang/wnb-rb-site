fbfd7757451c05203e8ca553046f4133545976c0
Ce Manalang <cecillemanalang@gmail.com>
2021-11-14 01:34:53 +0800

    Add /admin namespace

--- spec/system/admin_login_system_spec.rb
+++ spec/system/admin_login_system_spec.rb
@@ -6,58 +6,66 @@ RSpec.describe "Admin login system", type: :system do
     driven_by(:rack_test)
   end
 
-  context "admin not logged in" do
+  it "enables me to register to WNB.rb as admin" do
+    visit new_user_registration_path
 
+    fill_in "Your Name", with: "Person"
+    fill_in "Email", with: "admin@wnb.rb"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_text("Welcome!")
+  end
+
+  context "admin registered" do
     let(:email) { "admin@wnb.rb" }
     let(:password) { "password" }
 
-    before do
+    let(:user) do
       create(:user, email: email, password: password)
     end
 
-    it "enables me to login to WNB.rb admin" do
-      visit "/admin"
+    context "admin not logged in" do
 
-      fill_in "Email", with: email
-      fill_in "Password", with: password
-      click_button "Log in"
+      it "enables me to login to WNB.rb admin" do
+        visit "/admin"
 
-      expect(page).to have_text("successfully logged in")
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Log in"
+
+        expect(page).to have_text("Signed in successfully")
+      end
+
     end
 
-    it "enables me to register to WNB.rb as admin" do
-      visit "/admin/sign_up"
+    context "admin is logged in" do
 
-      fill_in "Email", with: "admin@wnb.rb"
-      fill_in "Password", with: "password"
-      fill_in "Confirm Password", with: "password"
-      click_button "Register"
+      it "enables me to logout from WNB.rb admin" do
+        login_as user
 
-      expect(page).to have_text("successfully registered")
+        visit admin_dashboard_path
+
+        click_on "Sign out"
+
+        expect(page).to have_current_path(root_path)
+      end
+
     end
 
-  end
+    it "enables me to reset WNB.rb admin password" do
+      token = user.send_reset_password_instructions
 
-  context "admin is logged in" do
+      visit edit_user_password_path(reset_password_token: token)
 
-    it "enables me to logout from WNB.rb admin" do
-      visit "/admin/sign_out"
+      fill_in "New password", with: "password1"
+      fill_in "Confirm new password", with: "password1"
 
-      expect(page).to have_text("successfully logged out")
+      click_button "Change my password"
+
+      expect(page).to have_text("Your password has been changed successfully")
     end
-
-  end
-
-  it "enables me to reset WNB.rb admin password" do
-    visit "/admin/password/new"
-
-    fill_in "Current Password", with: "password"
-    fill_in "New Password", with: "password1"
-    fill_in "Confirm New Password", with: "password1"
-
-    click_button "Reset Password"
-
-    expect(page).to have_text("successfully reset password")
   end
 
 end
